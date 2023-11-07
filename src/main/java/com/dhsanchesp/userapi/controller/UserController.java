9c3be750ac669ed86e8706f5317eaa4756d65e9a
Daniel Sanches <dh.sanchesp@gmail.com>
2023-11-06 23:08:01 -0300

    Add Swagger docs

--- src/main/java/com/dhsanchesp/userapi/controller/UserController.java
+++ src/main/java/com/dhsanchesp/userapi/controller/UserController.java
@@ -1,23 +1,26 @@
 package com.dhsanchesp.userapi.controller;
 
 import com.dhsanchesp.userapi.command.user.GetUserCommand;
+import com.dhsanchesp.userapi.dto.UserDto;
 import com.dhsanchesp.userapi.handler.BusHandler;
-import com.dhsanchesp.userapi.model.UserAddress;
+import com.dhsanchesp.userapi.repository.UserRepository;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.AllArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.dhsanchesp.userapi.dto.UserDto;
-import com.dhsanchesp.userapi.repository.UserRepository;
-
-import jakarta.websocket.server.PathParam;
-
 import static org.springframework.http.ResponseEntity.ok;
 
+@OpenAPIDefinition(info = @Info(summary = "User Controller"))
 @AllArgsConstructor
 @RestController
 @RequestMapping("user")
@@ -25,12 +28,19 @@ public class UserController {
 
     private final BusHandler busHandler;
     private UserRepository userRepository;
-    
+
     @GetMapping()
     public UserDto getUser() {
         return new UserDto("123ABC", "Luke", "Skywalker");
     }
 
+    @Operation(summary = "Get User by ID")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "OK",
+                    content = { @Content(mediaType = "application/json",
+                    schema = @Schema(implementation = UserDto.class)) }),
+            @ApiResponse(responseCode = "404", description = "Not Found", content = @Content)
+    })
     @GetMapping("/{userId}")
     public ResponseEntity<UserDto> getUserById(@PathVariable(value = "userId") final String userId) {
 
